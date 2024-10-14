cba96fe3d4713940cf58aa1715a97a82abbfb2b8
bip <bip@bip.bip>
2024-10-14 16:03:49 +0200

    n/a

---
+++ classes/random_classes/substract.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int subtract(int a, int b) {
+    int c;
+    c = a - b;
+    return c;
+}
+
+int main() {
+    int a = 10;
+    int b = 5;
+    subtract(a, b); // my start
+    return 0;
+}
